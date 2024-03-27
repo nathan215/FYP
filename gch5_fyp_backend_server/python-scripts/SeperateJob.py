ff53fb1e67d2c6e4d2b9cfc09e97baec4ea9114c
nathan215 <45839865+nathan215@users.noreply.github.com>
2024-03-27 15:21:13 +0800

    Update

--- gch5_fyp_backend_server/python-scripts/SeperateJob.py
+++ gch5_fyp_backend_server/python-scripts/SeperateJob.py
@@ -1,6 +1,7 @@
 # This is the main file to call the functions in the other files
 import threading
 from Data_handle.MQTT_Station import setup_station_mqtt
+from Data_handle.MQTT_Drone import setup_drone_mqtt # TODO: Implement this function
 from Data_handle.DataGenerate import start_generating_data
 from Data_handle.RealTimeDataCombine import start_combining_data
 from Data_handle.Simul_get_rssi import background_drone_add_rssi
@@ -9,11 +10,11 @@ from Process.Algorithm_run import run_algorithm
 from Process.trace_setting import trace_setting
 from shared_state import drone_data, station_data, combined_data, initial_location, device_id
 
-def testing():
-    # Start data generation and processing
-    start_generating_data()
-    start_combining_data()
-    # Your coordination logic here
+# def testing():
+#     # Start data generation and processing
+#     start_generating_data()
+#     start_combining_data()
+#     # Your coordination logic here
 
 def main():
     # Start the MQTT thread
@@ -26,12 +27,14 @@ def main():
     while True:
         # ask for user input to do data collection or testing
         user_input = input("Do you want to do data collection or testing? (data/testing): ")
+
+        # Start data collection
         if user_input == "data":
             if not device_id or not drone_data:
                 print("No device data received yet, please wait for a moment.")
                 continue
             combine_thread = threading.Thread(target=start_combining_data)
-
+        # Start testing
         elif user_input == "testing":
             trace_setting()
             combine_thread = threading.Thread(target=start_combining_data)
